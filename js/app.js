029dc7bf0111b47360743a64bb5287f78c9227d5
ojedam13 <m_ojeda17@hotmail.com>
2022-01-17 21:00:31 -0300

    validando formulario y mostrando el mensaje de error

--- js/app.js
+++ js/app.js
@@ -10,7 +10,37 @@ function buscarClima(e){
     e.preventDefault();
 
     //Validar
+    const ciudad = document.querySelector('#ciudad').value;
+    const pais = document.querySelector('#pais').value;
 
+    if (ciudad === '' || pais === '') {
+        //mostrar error
+        mostarError('Ambos campos son obligatorios');
+
+        return;
+    }
     //consultar API
-    
+
+}
+
+function mostarError(mensaje) {
+    const alerta = document.querySelector('.bg-red-100')
+    if (!alerta) {
+        //crear alerta
+        const alerta = document.createElement('DIV');
+
+        alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center');
+
+        alerta.innerHTML = `
+    <strong class="font-bold">Error!</strong>
+    <span class="block">${mensaje}</span>
+    `;
+        container.appendChild(alerta);
+
+        //elinimar alerta desp de 5seg
+        setTimeout(() => {
+            alerta.remove();
+        }, 3000);
+    }
+
 }
